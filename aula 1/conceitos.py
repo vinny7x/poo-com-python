ae177fb4222437f7337578b2055a4cd97eb84f98
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-29 13:27:06 -0300

    conceitos e operadores

---
+++ aula 1/conceitos.py
@@ -0,0 +1,11 @@
+print('hello world')
+print('POO')
+
+# # string
+nome = "Vinicios Mendes"
+# # inteiro
+idade = 19
+print(nome,idade)
+
+nome = input('Qual o seu nome?')
+print("Seu nome é ", nome)
---
+++ aula 1/operadores.py
@@ -0,0 +1,31 @@
+n1 = 7
+n2 = 9
+aprovado = True
+media = 6
+minima = 7
+
+
+soma = n1+n2
+subtracao = n1-n2
+multiplicacao = n1*n2
+divisao = n1/n2
+print('Operadores Aritméticos')
+print("Soma: ",soma)
+print("Subtração: ",subtracao)
+print("Multiplicação: ",multiplicacao)
+print("Divisão: ",divisao)
+
+print()
+print("Operadores Relacionais")
+print("Igualdade: ",n1==n2)
+print("Menor que: ",n1<n2)
+print("Maior que: ", n1>n2)
+print("Menor ou igual: ",n1<=n2)
+print("Maior ou igual: ", n1>=n2)
+
+print()
+print("Operadores Lógicos")
+print(aprovado)
+print(not aprovado)
+print(n1 >= 7 and minima == 7)
+print(n1 > 8 or minima == 7)
