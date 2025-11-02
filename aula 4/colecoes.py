707ce11edf04ba82c6bb5da15f18ef0e9b61da06
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-02 12:06:37 -0300

    coleções, conjuntos, tuplas e dicionários

---
+++ aula 4/colecoes.py
@@ -0,0 +1,32 @@
+# conjuntos
+conjunto = set([4, 7, 2, 3, 0, 8])
+print('Conjunto: ',conjunto)
+
+# tupla
+tupla = (3, 2, 4, 6, 0)
+print('Tupla: ', tupla)
+
+# dicionários
+pessoa = {
+    'nome': 'Vinicios', 
+    'telefone': '99 999999999', 
+    'endereco': 'ABC'
+    }
+
+pessoas = [
+    {
+    'nome': 'Vinicios', 
+    'telefone': '99 999999999', 
+    'endereco': 'ABC'
+    },
+    {
+    'nome': 'Carlos', 
+    'telefone': '99 987654321', 
+    'endereco': 'ABC'
+    },
+    {
+    'nome': 'Maria', 
+    'telefone': '99 123456789', 
+    'endereco': 'ABC'
+    }
+]
