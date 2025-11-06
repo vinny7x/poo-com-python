0fe01c7da4494311611fe9b205a7fa4b19e27528
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-06 15:14:35 -0300

    métodos

--- unidade 2/aula 3/Produto.py
+++ unidade 2/aula 3/Produto.py
@@ -1,13 +1,33 @@
 class Produto:
-    def __init__(self, nome, valor, marca = '', modelo = ''):
+    def __init__(self, nome, valor, quantidade = 0, marca = '', modelo = ''):
         self.nome = nome
         self.valor = valor
+        self.quantidade = quantidade
         self.marca = marca
         self.modelo = modelo
 
-produto0 = Produto('Celular', 3000, 'Samsung', 'J8 Core')
-produto1 = Produto('Geladeira', 10000, 'Brastemp', 'BRST9000')
-produto2 = Produto('Notebook', 3600, 'Acer')
+    def vender(self, quantidade):
+        if(quantidade > self.quantidade):
+            print('**********')
+            print('Não há estoque suficiente!')
+            print('Quantidade máxima:', self.quantidade)
+            print('**********')
+        else: 
+            self.quantidade -= quantidade
+
+    def comprar(self, quantidade):
+        self.quantidade += quantidade
+
+produto0 = Produto('Celular', 3000, 100, 'Samsung', 'J8 Core')
+produto0.comprar(10)
+produto0.vender(110)
+
+produto1 = Produto('Geladeira', 10000, 50,  'Brastemp', 'BRST9000')
+
+produto2 = Produto('Notebook', 3600, )
+produto2.comprar(100)
+produto2.vender(9)
+
 print(produto0.__dict__)
 print(produto1.__dict__)
 print(produto2.__dict__)
