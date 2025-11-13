794e9f1c0f8cbbe7d156563c602b1ee5021c333c
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-13 18:59:42 -0300

    métodos estáticos e de classe

--- unidade 3/aula 1/Imovel.py
+++ unidade 3/aula 1/Imovel.py
@@ -1,4 +1,5 @@
 class Imovel:
+    imposto = 0.02 # atributo de classe
     def __init__(self, nome, quartos, suites):
         self.nome = nome
         self.quartos = quartos
@@ -18,12 +19,29 @@ class Imovel:
     def __str__(self):
         return str(self.__dict__)
 
+    def detalhar(self):
+        return self.__dict__
+    def somarAposentos(self):
+        return self.quartos + self.suites
+    @staticmethod
+    def metodoEstatico():
+        print('Chamou o método estático sem criar o objeto')
+    @classmethod
+    def metodoDeClasse(cls):
+        print('Chamou o método de classe que vê o imposto', cls.imposto)
+
+
 casarao = Imovel('Casarão', 3, 4)
 mansao = Imovel('Mansão', 4, 5)
 
+print(casarao.somarAposentos())
+print(mansao.somarAposentos())
+Imovel.metodoEstatico() # método estático
+Imovel.metodoDeClasse() # método de classe
+
 # print(casarao.__dict__)
 # print(mansao.__dict__)
-print(casarao + mansao) # add
-print(casarao > mansao) # gt
-print(casarao < mansao) # lt
-print(casarao) # str
+# print(casarao + mansao) # add
+# print(casarao > mansao) # gt
+# print(casarao < mansao) # lt
+# print(casarao) # str
