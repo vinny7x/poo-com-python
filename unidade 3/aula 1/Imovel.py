efce6e2ab6c6b756479f1246056b29905cbb9596
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-13 19:13:11 -0300

    composição

--- unidade 3/aula 1/Imovel.py
+++ unidade 3/aula 1/Imovel.py
@@ -1,9 +1,21 @@
+class Categoria:
+    def __init__(self, tipo = ''):
+        self.tipo = tipo
+
+    def taxaAgua(self, consumo):
+        match self.tipo:
+            case 'Clínica': return consumo * 1
+            case 'Restaurante': return consumo * 2
+            case 'Hotel': return consumo * 2.5
+
+
 class Imovel:
     imposto = 0.02 # atributo de classe
     def __init__(self, nome, quartos, suites):
         self.nome = nome
         self.quartos = quartos
         self.suites = suites
+        self.categoria = Categoria()
     def __add__(self, other):
         somaSelf = self.quartos + self.suites
         somaOther = other.quartos + other.suites
@@ -30,14 +42,17 @@ class Imovel:
     def metodoDeClasse(cls):
         print('Chamou o método de classe que vê o imposto', cls.imposto)
 
+categoria = Categoria('Hotel')
+hotel1 = Imovel('Hotel do Pedro', 0, 150)
+hotel1.categoria = categoria
+print(hotel1.categoria.taxaAgua(300))
 
-casarao = Imovel('Casarão', 3, 4)
-mansao = Imovel('Mansão', 4, 5)
-
-print(casarao.somarAposentos())
-print(mansao.somarAposentos())
-Imovel.metodoEstatico() # método estático
-Imovel.metodoDeClasse() # método de classe
+# casarao = Imovel('Casarão', 3, 4)
+# mansao = Imovel('Mansão', 4, 5)
+# print(casarao.somarAposentos())
+# print(mansao.somarAposentos())
+# Imovel.metodoEstatico() # método estático
+# Imovel.metodoDeClasse() # método de classe
 
 # print(casarao.__dict__)
 # print(mansao.__dict__)
