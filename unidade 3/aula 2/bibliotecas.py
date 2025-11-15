cc13f3277dbdab611d5dabbc5f2e2c47333eea6f
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-15 20:41:01 -0300

    bibliotecas padrão

---
+++ unidade 3/aula 2/bibliotecas.py
@@ -0,0 +1,10 @@
+import math
+import random
+nota = 6.8
+
+print(math.floor(nota)) # arredonda para baixo
+print(math.ceil(nota)) # arredonda para cima
+print(math.pi) # retorna o PI
+
+print(random.random() * 100) # número aleatório
+print(random.randint(1, 100)) # número aleatório inteiro entre 1 2 100
