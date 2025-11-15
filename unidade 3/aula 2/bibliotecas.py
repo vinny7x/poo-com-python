c28696b1354c37240bffe54e4086de6d75d26806
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-15 20:41:40 -0300

    bibliotecas padrão

--- unidade 3/aula 2/bibliotecas.py
+++ unidade 3/aula 2/bibliotecas.py
@@ -7,4 +7,4 @@ print(math.ceil(nota)) # arredonda para cima
 print(math.pi) # retorna o PI
 
 print(random.random() * 100) # número aleatório
-print(random.randint(1, 100)) # número aleatório inteiro entre 1 2 100
+print(random.randint(1, 100)) # número aleatório inteiro entre 1 e 100
